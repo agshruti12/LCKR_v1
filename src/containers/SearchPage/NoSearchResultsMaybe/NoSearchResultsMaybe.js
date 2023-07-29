390b68fde455299bbb23a9dde1ebc8d018fded22
Shruti Agarwal <agshruti@Shrutis-MBP.fios-router.home>
2023-07-29 16:43:30 -0400

    no search result form

--- src/containers/SearchPage/NoSearchResultsMaybe/NoSearchResultsMaybe.js
+++ src/containers/SearchPage/NoSearchResultsMaybe/NoSearchResultsMaybe.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { FormattedMessage } from '../../../util/reactIntl';
 import css from './NoSearchResultsMaybe.module.css';
+import { ExternalLink } from '../../../components';
 
 const NoSearchResultsMaybe = props => {
   const { listingsAreLoaded, totalItems, location, resetAll } = props;
+  console.log('arrived no search results');
+  console.log(props);
   const hasNoResult = listingsAreLoaded && totalItems === 0;
   const hasSearchParams = location.search?.length > 0;
   return hasNoResult ? (
     <div className={css.noSearchResults}>
       <FormattedMessage id="SearchPage.noResults" />
+      <br />
+      <br />
+      <ExternalLink href="https://forms.gle/wGqfgTTm8ShJWiub6">
+        Can't find what you're looking for? Click here!
+      </ExternalLink>
       <br />
       {hasSearchParams ? (
         <button className={css.resetAllFiltersButton} onClick={e => resetAll(e)}>
